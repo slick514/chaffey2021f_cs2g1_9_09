6916fa2df1936372904e8c2dbd5c117aafceff44
Justin Gries <justin.gries@gmail.com>
2021-10-12 21:06:05 -0700

    initial check-in for 9.09

---
+++ main.cpp
@@ -0,0 +1,15 @@
+using namespace std;
+
+/*
+ * Course: Chaffey College 2021-Fall CS2
+ * Prof: Dr. Tracy Kocher
+ * Date:
+ * Project:
+ * Coder: Justin Gries
+ * Tester(s):
+ *
+ * Prompt:
+ */
+int main(int argc, char **argv) {
+    return 0;
+}
